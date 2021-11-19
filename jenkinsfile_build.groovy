2fcab38518f286eb58c69df79261dcdfffa70c26
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-19 16:57:25 +0530

    add checkout command

--- jenkinsfile_build.groovy
+++ jenkinsfile_build.groovy
@@ -5,8 +5,8 @@ pipeline{
             steps{
                 println "clone our code to our repository"
                 sh "ls -l"
-                //checkout([$class: 'GitSCM', branches: [[name: '*/branchname']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/sivalakshmanna/boxfuse-sample-java-war-hello.git']]])
-                //sh "ls -lart ./*"
+                checkout([$class: 'GitSCM', branches: [[name: '*/branchname']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/sivalakshmanna/boxfuse-sample-java-war-hello.git']]])
+                sh "ls -lart ./*"
 
             }
         }
