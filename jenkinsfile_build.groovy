6231469f2b3054d5cb894247d4a3709b33f16b18
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-19 15:31:53 +0530

    addedfile

---
+++ jenkinsfile_build.groovy
@@ -0,0 +1,20 @@
+pipeline{
+    agent any
+    stages{
+        stage("checkout code"){
+            steps{
+                println "clone our code to our repository"
+            }
+        }
+        stage("build code"){
+            steps{
+                println "mvn clean package"
+            }
+        }
+        stage("upload artifacts to s3"){
+            steps{
+                println "uploading artifacts to s3 bucket"
+            }
+        }
+    }
+}
