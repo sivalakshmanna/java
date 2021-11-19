ce96e3c6f302298f95951c1f504c9fc2aabe80ac
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-19 17:01:17 +0530

    updated bild command

--- jenkinsfile_build.groovy
+++ jenkinsfile_build.groovy
@@ -5,7 +5,7 @@ pipeline{
             steps{
                 println "clone our code to our repository"
                 sh "ls -l"
-                checkout([$class: 'GitSCM', branches: [[name: '*/branchname']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/sivalakshmanna/boxfuse-sample-java-war-hello.git']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/sivalakshmanna/boxfuse-sample-java-war-hello.git']]])
                 sh "ls -lart ./*"
 
             }
