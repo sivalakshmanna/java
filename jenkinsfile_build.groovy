1061a36e24429b73782699176eb8b6e0dcdecd7a
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-19 16:51:09 +0530

    modified

--- jenkinsfile_build.groovy
+++ jenkinsfile_build.groovy
@@ -5,6 +5,9 @@ pipeline{
             steps{
                 println "clone our code to our repository"
                 sh "ls -l"
+                //checkout([$class: 'GitSCM', branches: [[name: '*/branchname']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/sivalakshmanna/boxfuse-sample-java-war-hello.git']]])
+                //sh "ls -lart ./*"
+
             }
         }
         stage("build code"){
